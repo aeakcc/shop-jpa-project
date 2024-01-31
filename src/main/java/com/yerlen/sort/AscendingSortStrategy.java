17740ec571c0082566399928cffa9a395003c050
aeakcc <aeakcc@gmail.com>
2024-01-31 22:05:24 +0600

    feature/JPA-2: add comment

--- src/main/java/com/yerlen/sort/AscendingSortStrategy.java
+++ src/main/java/com/yerlen/sort/AscendingSortStrategy.java
@@ -8,4 +8,5 @@ public class AscendingSortStrategy <T extends ParentEntity> implements SortStrat
     public List<T> sort(List<T> items) {
         return items.stream().sorted().toList();
     }
+    // lol che
 }
